3d5bcc3394edab27b53721e3c2415240bb78489d
Mehdi Bateni <58263132+mmbateni@users.noreply.github.com>
2026-05-13 15:07:01 -0700

    Fix missing library and add comments for ordinal translation
    Added missing library for reading Excel files and included comments for MATLAB translation.

--- optimum_lag_tele.R
+++ optimum_lag_tele.R
@@ -1,3 +1,6 @@
+# BUG FIX 5: Missing library loaded.
+library(openxlsx)
+
 ind_xls <- read.xlsx("index_basin.xlsx", sheet = "ind", startRow = 2, endCol = 3)
 ind_xls_t_bsn <- ind_xls[, 1]
 n_bs <- length(ind_xls_t_bsn)
@@ -6,6 +9,11 @@ n_scale <- length(scale)
 overlap <- n_scale - 1
 bc <- ncol(best_cop_indx)
 bd <- nrow(best_cop_indx)
+
+# BUG FIX 5: MATLAB's ordinal() translation using cut()
+# Example replacement pattern for ordinal variables:
+# Replace "my_breaks" and "my_labels" with the actual thresholds you were using in MATLAB
+# ordinal_data <- cut(best_cop_indx, breaks = my_breaks, labels = my_labels, ordered_result = TRUE)
 
 ## Rest of the code involves mostly data manipulation and calculations.
 ## Please make sure to use appropriate functions in R for file reading and saving.
